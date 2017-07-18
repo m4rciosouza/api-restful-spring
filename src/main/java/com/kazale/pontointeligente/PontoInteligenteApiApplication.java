4555393b8410808314c55c2ec2d9bd9ebf6cc618
Marcio <marcio@kazale.com>
2017-07-18 11:25:40 -0300

    Arquivos iniciais do projeto.

---
+++ src/main/java/com/kazale/pontointeligente/PontoInteligenteApiApplication.java
@@ -0,0 +1,12 @@
+package com.kazale.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApiApplication.class, args);
+	}
+}
